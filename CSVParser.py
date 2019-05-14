0cad0ba84bf647b5b3f6c8ea88677108b43d9d69
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-05-14 11:29:41 +0200

    Updated load_csv def to return a list

--- CSVParser.py
+++ CSVParser.py
@@ -2,23 +2,23 @@ import csv
 
 
 # Read the contents from csv file and return a dict
-def load_csv(csv_file: str):
+def load_csv(csv_file: str) -> list:
     with open(csv_file, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
 
-        # for logging
+        crime_dict = []
         line_count = 0
-        for row in csv_reader:
 
+        for row in csv_reader:
+            crime_dict.append(row)
             # Columns
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
 
             print(f'Crime: {row["crimedescr"]}')
-        # end of logging
 
-        return csv_reader
+        return crime_dict
 
 
 if __name__ == '__main__':
