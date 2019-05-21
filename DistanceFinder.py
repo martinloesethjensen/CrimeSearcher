30cf5402c727e590ad29e6085fd75915e163adaf
redrocket94 <Oliver.duedam@gmail.com>
2019-05-21 15:14:12 +0200

    cleaned distancefinder/gps with no outcommented code for easysort functionality

--- DistanceFinder.py
+++ DistanceFinder.py
@@ -5,8 +5,21 @@ def __get_lon_lat():
     import json
     with urlopen("http://ipinfo.io/json") as url:
         json_data = json.loads(url.read().decode())
-        return str(json_data["loc"])
+        temp_var = (json_data["loc"]).split(",")
+        temp_var = [float(data) for data in temp_var]
+        return tuple(temp_var)
+
 
 
 def calculate_distance(other_loc):
-    print(great_circle(__get_lon_lat(), str(other_loc)).km)
+    distance = great_circle(__get_lon_lat(), other_loc)
+    return distance
+
+
+def get_scanned_locations_list(data_list):
+    scanned_list = []
+    for item in data_list:
+        coords = (item["latitude"], item["longitude"])
+        if calculate_distance(coords) < 5.0:
+            scanned_list.append(item)
+    return (scanned_list)
